bc20dd8ca664bb01daebf6a438704c932869ab47
ZhukovWang <zhukov.wang@outlook.com>
2020-07-26 13:25:34 +0800

    feat: add version information file.

---
+++ PodcastDownloadManager/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("Podcast Download Manager")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Podcast Download Manager")]
+[assembly: AssemblyCopyright("Copyright © ZhukovWang 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
